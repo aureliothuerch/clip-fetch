f95888568b8e1681849c19ec8e787077e0053b94
aureliothuerch <contact@aurelio.swiss>
2026-09-24 22:03:11 +0200

    feat: base cli logic

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,15 @@ use std::{env, println};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    println!("{}", args[0]);    
+    if args.len() > 1 {
+        if !args[1].contains("https://") {
+            println!("This is an invalid link")
+        }
+        else {
+            println!("The URL is: {}", args[1]);
+        }
+    }
+    else {
+        println!("Please put a link: cf <link>")
+    }
 }
